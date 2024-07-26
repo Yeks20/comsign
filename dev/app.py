74139fb09eca189db65010068c0b0e27a21edec3
OLAYEMI <127778682+Yeks20@users.noreply.github.com>
2024-07-26 14:04:05 -0500

    Add files via upload

--- dev/app.py
+++ dev/app.py
@@ -1,31 +1,32 @@
-from flask import Flask, render_template, Response, jsonify 
+from flask import Flask, render_template, Response, jsonify
 import torch
-from torchvision import transforms, models  
+from torchvision import transforms, models
+from torchvision.models import MobileNet_V2_Weights
 from PIL import Image
 import cv2
 import numpy as np
-import pandas as pd
 import time
-import pickle
-import warnings
-warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 def initialize_model(num_classes):
-    model = models.mobilenet_v2(pretrained=False)
+    model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
     model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, num_classes)
     model = torch.nn.DataParallel(model)
     return model
 
+# Define your classes here
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+
+# Create label map
 def create_label_map(df):
     labels = df['label'].unique()
     label_map = {label: idx for idx, label in enumerate(labels)}
     return label_map
 
-def predict_image(image, model, transform, label_map):
+def predict_image(image, model, transform):
     model.eval()
     image = transform(image).unsqueeze(0)  # Add batch dimension
     image = image.to(device)
@@ -34,22 +35,21 @@ def predict_image(image, model, transform, label_map):
         _, predicted = torch.max(outputs, 1)
     return predicted.item()
 
-train_csv = r"C:\Users\HP\Documents\courses_conestoga\comsign\data-collection\Training_set.csv"
-df = pd.read_csv(train_csv)
-label_map = create_label_map(df)
+label_map = {label: idx for idx, label in enumerate(classes)}
 reverse_label_map = {idx: label for label, idx in label_map.items()}
 
-
+# Define the image transformations
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-num_classes = len(label_map)
+num_classes = len(classes)
 model = initialize_model(num_classes)
-model.load_state_dict(torch.load(r"C:\Users\HP\Documents\courses_conestoga\comsign\training\mobilenet_model.pth", map_location=device))
+model.load_state_dict(torch.load('./training/mobilenet_model.pth', map_location=device))
 model.to(device)
+
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -59,9 +59,9 @@ text = ""
 last_prediction_time = time.time()
 prediction_interval = 2
 
-
 def gen_frames():
     global text, last_prediction_time
+    predicted_label = "No prediction yet" 
     while True:
         success, frame = cap.read()
         if not success:
@@ -71,7 +71,7 @@ def gen_frames():
             if current_time - last_prediction_time >= prediction_interval:
                 # Convert captured frame to PIL Image
                 pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-                predicted_label_idx = predict_image(pil_image, model, transform, label_map)
+                predicted_label_idx = predict_image(pil_image, model, transform)
                 predicted_label = reverse_label_map[predicted_label_idx]
 
                 # Update text based on predicted label
@@ -93,15 +93,13 @@ def gen_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
-
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/text')
 def get_text():
@@ -109,4 +107,4 @@ def get_text():
     return text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,)
